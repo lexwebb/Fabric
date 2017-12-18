700ea7b0b693c08d640ef1e2837b64c97c961e7a
Lex Webb <lexwebb@gmail.com>
2017-12-18 23:18:06 +0000

    Initial Commit

---
+++ Fabric.Tests/CoreTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace Fabric.Tests
+{
+    public class CoreTests
+    {
+        [Fact]
+        public void CoreInitialise_Throws_NotImplementedException() {
+            var core = Core.Instance;
+
+            Assert.Throws<NotImplementedException>(() => core.Initialise());
+        }
+    }
+}
